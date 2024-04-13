b32c3bfe8d1e81a496353329e3557443a1ce4893
Junaid <muhmmadjunaid763@gmail.com>
2024-04-13 23:51:05 +0500

    added comment

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <RouterProvider router={router} /> */}
+    {/* Testing commit */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
